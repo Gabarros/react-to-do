e9c5657662ea05277e05be8fdfeffa1a90ae638e
Gabarros <gabarros17@gmail.com>
2019-11-09 16:19:44 -0300

    Create Main Layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
+
+import CreateTodo from './components/create-todo.component';
+import EditTodo from './components/edit-todo.component';
+import TodosList from './components/todos-list.component';
+
+import logo from './logo.svg';
 
 class App extends Component {
 
@@ -11,9 +16,29 @@ class App extends Component {
     return (
 
       <Router>
+        <div className="container">
+
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com"></img>
+            </a>
+            <Link to="/" className="navbar7" >MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item" >
+                  <Link to="/create" className="nav-link">Create Todos</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
         <Route path="/" exact component={TodosList}/>
-        <Route path="/edit/:id" exact component={EditList}/>
+        <Route path="/edit/:id" exact component={EditTodo}/>
         <Route path="/create" exact component={CreateTodo}/>
+        </div>
       </Router>
 
     );
---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class EditTodo extends Component{
+
+    render(){
+
+        return(
+            <div>
+                <p>Welcome to edit todo components!</p>
+            </div>
+        )
+    }
+
+}
