ea8733bcd39593891522553c6f32f7fe735aa032
Gabarros <gabarros17@gmail.com>
2019-11-10 16:41:31 -0300

    Create Update todo route

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,28 @@ todoRoutes.post('/add', (req, res)=>{
     })
 });
 
+todoRoutes.post('/update/:id', (req, res)=>{
+
+    Todo.findById(req.params.id, function(err, todo){
+
+        if(!todo){
+            res.status(404).send("Data not found");
+        }else{
+
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo=>{
+                res.json('Todo updated!');
+            }).catch(err=>{
+                res.status(400).send('Update not possible');
+            });
+        };
+    });
+});
+
 // todoRoutes.route('/').get(function(req, res){
 //     Todo.find(function(err, todos){
 //         if(err){
